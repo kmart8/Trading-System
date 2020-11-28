5079952f32f65b2f7f10c5569ce23c76db08e04b
Kevin Martin <kevinmartin@Kevins-MBP.fios-router.home>
2020-11-28 18:39:10 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Markup
+
+# Kevin's files
+from Kevin import *
+from Kevin.backtest import *
+
+# Wenlei's files
+from Wenlei.TRIMA_wenlei_cao_get_backtrade_result import *
+
+# Expectunity Calculation
+from expectunityCalc import *
+
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
+
+
+@app.route("/results")
+def results():
+    backtest()
+    expectunityKevin = combined_expectunity_calcuation(
+        "/Users/kevinmartin/Documents/Fall '20/GQP/Trading System/result.csv", 1000)
+    main()
+    expectunityWenlei = combined_expectunity_calcuation(
+        "/Users/kevinmartin/Documents/Fall '20/GQP/Trading System/result.csv", 100)
+    return render_template("results.html", expectunityKevin=Markup(expectunityKevin),
+                           expectunityWenlei=Markup(expectunityWenlei))
 
 
 if __name__ == '__main__':
